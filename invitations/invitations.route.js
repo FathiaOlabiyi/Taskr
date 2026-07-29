7a030b16de17e414c9774700fdb48a0e4a4fb2fd
Fathia Olabiyi <fathiatolabiyi02@gmail.com>
2026-07-29 17:06:19 +0100

    twelveth commit

--- invitations/invitations.route.js
+++ invitations/invitations.route.js
@@ -7,20 +7,14 @@ const Router = express.Router();
 
 Router.post("/:id/invitation", validateToken, isMember, hasPermission("Manage Invitation"), createInvitation);
 Router.post("/:id/invitation/:invitationId/send", validateToken, isMember, hasPermission("Manage Invitation"), sendInvitation);
+Router.post("/:id/invitation/:invitationId/resend", validateToken, isMember, hasPermission("Manage Invitation"), resendInvitation);
 Router.get("/:id/invitation", getInvitation);
+Router.get("/:id/invitation/all", validateToken, isMember, hasPermission("Manage Invitation"), getInvitations);
+Router.get("/:id/invitation/:invitationId", validateToken, isMember, hasPermission("Manage Invitation"), getInvitationById);
 Router.patch("/:id/invitation/accept", validateToken, acceptInvitation);
 Router.patch("/:id/invitation/reject", validateToken, rejectInvitation);
 Router.patch("/:id/invitation/:invitationId/revoke", validateToken, isMember, hasPermission("Manage Invitation"), revokeInvitation);
 Router.patch("/:id/invitation/:invitationId/reschedule", validateToken, isMember, hasPermission("Manage Invitation"), rescheduleInvitation);
-Router.post("/:id/invitation/:invitationId/resend", validateToken, isMember, hasPermission("Manage Invitation"), resendInvitation);
-Router.get("/:id/invitation/all", validateToken, isMember, hasPermission("Manage Invitation"), getInvitations);
-Router.get(
-  "/:id/invitation/:invitationId",
-  validateToken,
-  isMember,
-  hasPermission("Manage Invitation"),
-  getInvitationById,
-);
 Router.patch("/:id/invitation/:invitationId", validateToken, isMember, hasPermission("Manage Invitation"), updateInvitation);
 Router.patch("/:id/invitation/:invitationId/delete", validateToken, isMember, hasPermission("Manage Invitation"), deleteInvitation);
 
