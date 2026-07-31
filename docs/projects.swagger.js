376a66e49e7757bd6171f7f15b7fe62bee482bf3
FathiaOlabiyi <103903399+FathiaOlabiyi@users.noreply.github.com>
2026-07-31 11:35:05 +0100

    Added swagger documentation for project
    Updated Swagger documentation for project endpoints, changing the HTTP method for project deletion and modifying response codes.

--- docs/projects.swagger.js
+++ docs/projects.swagger.js
@@ -164,3 +164,145 @@
  *                       type: string
  *                     
  */
+
+
+//UPDATE PROJECT DETAILS BY ID
+/**
+ * @swagger
+ * /api/v1/project/{id}:
+ *   patch:
+ *     summary: Update project details by id
+ *     tags:
+ *       - Project
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema: 
+ *           type: string
+ *       
+ *     security:
+ *       - bearerAuth: []
+ *
+ *     requestBody:
+ *       description: Provide at least one field to update
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             minProperties: 1
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               dueDate:
+ *                 type: string
+ *                 format: date
+ *     responses:
+ *       201:
+ *         description: Update successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     ownerId:
+ *                       type: string
+ *                     title: 
+ *                       type: string
+ *                     description: 
+ *                       type: string
+ *                     status: 
+ *                       type: string
+ *                     dueDate: 
+ *                       type: string
+ *                     _id: 
+ *                       type: string
+ *                     
+ */
+
+
+//UPDATE PROJECT STATUS
+/**
+ * @swagger
+ * /api/v1/project/{id}/status:
+ *   patch:
+ *     summary: Update project status
+ *     tags:
+ *       - Project
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema: 
+ *           type: string
+ *       
+ *     security:
+ *       - bearerAuth: []
+ *
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: true
+ *             properties:
+ *               status:
+ *                 type: string
+ *                 enum: ["planning", "active", "on_hold", "completed"]
+ *     responses:
+ *       201:
+ *         description: Update successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     ownerId:
+ *                       type: string
+ *                     title: 
+ *                       type: string
+ *                     description: 
+ *                       type: string
+ *                     status: 
+ *                       type: string
+ *                     dueDate: 
+ *                       type: string
+ *                     _id: 
+ *                       type: string
+ *                     
+ */
+
+
+//DELETE PROJECT
+/**
+ * @swagger
+ * /api/v1/project/{id}/delete:
+ *   patch:
+ *     summary: Delete a project
+ *     tags:
+ *       - Project
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema: 
+ *           type: string
+ *       
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       204:
+ *         description: Project deleted successfully
+ *                     
+ */
