116f2bc098a7feab91e5dc59fa3107e4b786ebb1
Fathia Olabiyi <fathiatolabiyi02@gmail.com>
2026-08-03 08:29:00 +0100

    home page url update

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get("/homepage", (req, res) => {
+app.get("/", (req, res) => {
     logger.info("Accessed Home Page")
     res.status(200).json("Welcome to Taskr Homepage")
 });
